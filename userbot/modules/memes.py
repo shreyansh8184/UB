09bfa06af47750cffae27ae0f37c693205422228
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-07 10:28:14 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -932,7 +932,7 @@ async def hack(hacked):
         if await hacked.get_reply_message():
             await hacked.edit(
                 "`Targeted User was Hacked successfully 👨‍💻......`"
-                "`Pay 999$ To` @devpatel_73 `To Remove This Hack`\n"
+                "`Pay 999$ To` my master `To Remove This Hack`\n"
             )
 
 			  
